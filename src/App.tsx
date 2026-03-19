d873d6ecc55a7744b377d7c14d8f52ffe0878729
Shatica R Miles <whosttv@gmail.com>
2026-03-19 16:45:15 -0400

    fixed build

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-<<<<<<< HEAD
-  <div style={{
-    width: "100%",
-    height: "100vh",
-    margin: 0,
-    padding: 0
-  }}>
-    <Index />
-  </div>
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
->>>>>>> d39ee8bb6ceea7edab19179109fa96784303e567
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+            <Toaster />
+               <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+          </QueryClientProvider>
+  );
+};
 
 export default App;
